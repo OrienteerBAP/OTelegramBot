aeca2ad746104317b269e0c406a4976f0978601a
weaxme <weaxme@gmail.com>
2016-10-27 17:48:31 +0300

    Add basic methods in class OTelegramBot

--- src/main/java/org/orienteer/telegram/OTelegramBot.java
+++ src/main/java/org/orienteer/telegram/OTelegramBot.java
@@ -1,32 +1,112 @@
 package org.orienteer.telegram;
 
+import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
 import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
 
 /**
- * Created by Vitaliy Gonchar on 27.10.16.
+ * @author Vitaliy Gonchar
  */
 public class OTelegramBot extends TelegramLongPollingBot {
 
+    private static final Logger LOG = LoggerFactory.getLogger(OTelegramBot.class);
+    private final OTelegramModule.BotConfig BOT_CONFIG;
+    private final ODatabaseDocument DATABASE;
+
+    public OTelegramBot(OTelegramModule.BotConfig botConfig, ODatabaseDocument db) {
+        BOT_CONFIG = botConfig;
+        DATABASE = db;
+    }
+
     @Override
     public String getBotToken() {
-        return null;
+        return BOT_CONFIG.TOKEN;
     }
 
     @Override
     public String getBotUsername() {
-        return null;
+        return BOT_CONFIG.USERNAME;
     }
-
-
-    @Override
-    public void onUpdateReceived(Update update) {
-
-    }
-
 
     @Override
     public void onClosing() {
+
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            Message message = update.getMessage();
+            if (message.hasText()) {
+                try {
+                    handleIncomingMessage(message);
+                } catch (TelegramApiException e) {
+                    LOG.error("Cannot send message");
+                    if (LOG.isDebugEnabled()) e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private void handleIncomingMessage(Message message) throws TelegramApiException {
+        BotState state = getBotState(message.getText());
+        SendMessage sendRequestMessage;
+        switch (state) {
+            case START:
+                sendRequestMessage = getTextMessage(message, "Orienteer bot");
+                break;
+            case INPUT:
+                sendRequestMessage = getTextMessage(message, "User input");
+                break;
+            case SEARCH:
+                sendRequestMessage = getTextMessage(message, "Search class");
+                break;
+            case GET:
+                sendRequestMessage = getTextMessage(message, "Get class");
+                break;
+            default:
+                sendRequestMessage = getTextMessage(message, "Error");
+                break;
+        }
+        sendMessage(sendRequestMessage);
+    }
+
+    private SendMessage getTextMessage(Message message, String text) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        return sendMessage;
+    }
+
+    private BotState getBotState(String text) {
+        BotState state = BotState.ERROR;
+        for (BotState search : BotState.values()) {
+            if (search.command.equals(text)) {
+                state = search;
+                break;
+            }
+        }
+
+        return state;
+    }
+
+    private enum BotState {
+        START("/start"),
+        INPUT("/input"),
+        SEARCH("/search"),
+        GET("/get"),
+        ERROR("__error");
+
+        private String command;
+        BotState(String command) {
+            this.command = command;
+        }
 
     }
 }
--- src/main/java/org/orienteer/telegram/OTelegramModule.java
+++ src/main/java/org/orienteer/telegram/OTelegramModule.java
@@ -1,5 +1,6 @@
 package org.orienteer.telegram;
 
+import com.orientechnologies.orient.core.iterator.ORecordIteratorClass;
 import com.orientechnologies.orient.core.metadata.schema.OClass;
 import org.orienteer.core.OrienteerWebApplication;
 import org.orienteer.core.module.AbstractOrienteerModule;
@@ -10,16 +11,25 @@ import com.orientechnologies.orient.core.record.impl.ODocument;
 import com.orientechnologies.orient.core.metadata.schema.OType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.logging.BotLogger;
+import org.telegram.telegrambots.logging.BotsFileHandler;
 
-import java.util.Collection;
+import java.io.IOException;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
 
+/**
+ * @author Vitaliy Gonchar
+ */
 public class OTelegramModule extends AbstractOrienteerModule{
 
 	private static final Logger LOG = LoggerFactory.getLogger(OTelegramModule.class);
 
 	public static final String OCLASS_NAME = "OTelegramBot";
 	public static final String OPROPERTY_USERNAME = "username";
-	public static final String OPROPERTY_TOKKEN = "tokken";
+	public static final String OPROPERTY_TOKEN = "token";
 
 	protected OTelegramModule() {
 		super("OTelegramModule", 1);
@@ -30,8 +40,8 @@ public class OTelegramModule extends AbstractOrienteerModule{
 		OSchemaHelper helper = OSchemaHelper.bind(db);
 		helper.oClass(OCLASS_NAME, "OModule")
 				.oProperty(OPROPERTY_USERNAME, OType.STRING)
-				.oProperty(OPROPERTY_TOKKEN, OType.STRING)
-				.switchDisplayable(true, OPROPERTY_USERNAME, OPROPERTY_TOKKEN);
+				.oProperty(OPROPERTY_TOKEN, OType.STRING)
+				.switchDisplayable(true, OPROPERTY_USERNAME, OPROPERTY_TOKEN);
 		LOG.info("Install");
 		//Install data model
 		//Return null of default OModule is enough
@@ -41,7 +51,16 @@ public class OTelegramModule extends AbstractOrienteerModule{
 	@Override
 	public void onInitialize(OrienteerWebApplication app, ODatabaseDocument db) {
 		createClass(db);
-		readBotConfig(db);
+		BotConfig botConfig = readBotConfig(db);
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+		BotLogger.setLevel(Level.WARNING);
+		BotLogger.registerLogger(new ConsoleHandler());
+		try {
+			telegramBotsApi.registerBot(new OTelegramBot(botConfig, db));
+		} catch (TelegramApiRequestException e) {
+			LOG.error("Cannot register bot");
+			if (LOG.isDebugEnabled()) e.printStackTrace();
+		}
 	}
 
 	protected void createClass(ODatabaseDocument db) {
@@ -50,23 +69,33 @@ public class OTelegramModule extends AbstractOrienteerModule{
 			OClass oModule = db.getMetadata().getSchema().getClass("OModule");
 			oTelegramClass.addSuperClass(oModule);
 			oTelegramClass.createProperty(OPROPERTY_USERNAME, OType.STRING);
-			oTelegramClass.createProperty(OPROPERTY_TOKKEN, OType.STRING);
+			oTelegramClass.createProperty(OPROPERTY_TOKEN, OType.STRING);
 		}
 	}
 
 	protected BotConfig readBotConfig(ODatabaseDocument db) {
-        BotConfig botConfig = null;
-
-        return botConfig;
+		ORecordIteratorClass<ODocument> oTelegramBots = db.browseClass(OCLASS_NAME);
+		String username = null;
+		String token = null;
+		if (oTelegramBots.hasNext()) {
+			ODocument bot = oTelegramBots.next();
+			if (bot.field(OMODULE_ACTIVATE)) {
+				username = bot.field("username");
+				token = bot.field("token");
+			}
+		}
+		LOG.info("Bot username: " + username);
+		LOG.info("Bot token: " + token);
+		return new BotConfig(username, token);
     }
 
-    private class BotConfig {
+    public class BotConfig {
         final String USERNAME;
-        final String TOKKEN;
+        final String TOKEN;
 
-        BotConfig(String username, String tokken) {
+        BotConfig(String username, String token) {
             USERNAME = username;
-            TOKKEN = tokken;
+            TOKEN = token;
         }
     }
 }
