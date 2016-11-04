ced7a77088a67af419d303ab71bc0f9624505eb5
weaxme <weaxme@gmail.com>
2016-11-04 16:20:54 +0200

    Create emptywidget

--- src/main/java/org/orienteer/telegram/MyWebApplication.java
+++ src/main/java/org/orienteer/telegram/MyWebApplication.java
@@ -13,6 +13,7 @@ public class MyWebApplication extends OrienteerWebApplication
 	{
 		super.init();
 		mountPages("org.orienteer.telegram.web");
+		registerWidgets("org.orienteer.telegram.widget");
 		registerModule(OTelegramModule.class);
 	}
 	
