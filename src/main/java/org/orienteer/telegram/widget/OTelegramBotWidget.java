b5e0d612a22036fae2d937185c45f2343f8ca7e2
weaxme <weaxme@gmail.com>
2016-11-06 23:05:53 +0200

    Add simple panel

--- src/main/java/org/orienteer/telegram/widget/OTelegramBotWidget.java
+++ src/main/java/org/orienteer/telegram/widget/OTelegramBotWidget.java
@@ -1,14 +1,15 @@
 package org.orienteer.telegram.widget;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
+import java.io.Serializable;
+import java.util.*;
 
 import com.orientechnologies.orient.core.id.ORecordId;
-import com.orientechnologies.orient.core.metadata.schema.OGlobalProperty;
-import com.orientechnologies.orient.core.metadata.schema.OSchema;
+import com.orientechnologies.orient.core.iterator.ORecordIteratorClass;
+import com.orientechnologies.orient.core.metadata.schema.*;
+import com.orientechnologies.orient.core.metadata.security.ORule;
 import com.orientechnologies.orient.core.sql.query.OSQLSynchQuery;
 import org.apache.wicket.Component;
+import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.*;
 import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.LoadableDetachableModel;
@@ -19,10 +20,15 @@ import org.orienteer.core.component.FAIconType;
 import org.orienteer.core.component.ODocumentPageLink;
 import org.orienteer.core.component.command.EditSchemaCommand;
 import org.orienteer.core.component.command.SaveSchemaCommand;
+import org.orienteer.core.component.meta.AbstractComplexModeMetaPanel;
 import org.orienteer.core.component.meta.AbstractModeMetaPanel;
 import org.orienteer.core.component.meta.OClassMetaPanel;
+import org.orienteer.core.component.property.BooleanEditPanel;
+import org.orienteer.core.component.property.BooleanViewPanel;
 import org.orienteer.core.component.property.DisplayMode;
 import org.orienteer.core.component.structuretable.OrienteerStructureTable;
+import org.orienteer.core.component.widget.oclass.OClassConfigurationWidget;
+import org.orienteer.core.component.widget.oclass.OClassHooksWidget;
 import org.orienteer.core.util.ODocumentChoiceRenderer;
 import org.orienteer.core.widget.AbstractModeAwareWidget;
 import org.orienteer.core.widget.Widget;
@@ -30,13 +36,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.orientechnologies.orient.core.db.document.ODatabaseDocument;
-import com.orientechnologies.orient.core.metadata.schema.OClass;
-import com.orientechnologies.orient.core.metadata.schema.OProperty;
 import com.orientechnologies.orient.core.record.impl.ODocument;
 
 import ru.ydn.wicket.wicketorientdb.OrientDbWebSession;
 import ru.ydn.wicket.wicketorientdb.components.TransactionlessForm;
 import ru.ydn.wicket.wicketorientdb.model.SimpleNamingModel;
+import ru.ydn.wicket.wicketorientdb.proto.OClassPrototyper;
+import ru.ydn.wicket.wicketorientdb.security.OSecurityHelper;
+import ru.ydn.wicket.wicketorientdb.security.OrientPermission;
 import ru.ydn.wicket.wicketorientdb.utils.DBClosure;
 
 /**
@@ -56,50 +63,147 @@ public class OTelegramBotWidget extends AbstractModeAwareWidget<OClass> {
 		super(id, model, widgetDocumentModel);
 		Form<OClass> form = new TransactionlessForm<OClass>("form");
 		test();
-		propertiesList.add("isEnableToSearch");
-		propertiesList.add("searchQuery");
+		propertiesList.add("telegramSearch");
+		propertiesList.add("telegramSearchQuery");
+
+		//modifySchema();
+		final OBotWidgetPopertiesClass pr = new OBotWidgetPopertiesClass("OClassTelegramState", getModel().getObject().getName());
 		structureTable = new OrienteerStructureTable<OClass, String>("attributes", model, propertiesList) {
-			
+
 			@Override
 			protected Component getValueComponent(String id, IModel<String> rowModel) {
-				return new AbstractModeMetaPanel<OClass, DisplayMode, String, ODocument>(id, getModeModel(), OTelegramBotWidget.this.getModel(), rowModel) {
+				return new OClassMetaPanel<Object>(id, getModeModel(), getModel(), rowModel) {
 					@Override
-					protected IModel<ODocument> resolveValueModel() {
-						return new LoadableDetachableModel<ODocument>() {
-							@Override
-							protected ODocument load() {
-								return null;
-							}
+					protected Object getValue(OClass entity, String critery) {
+						if (critery.equals(propertiesList.get(0))) {
+							return pr.isTelegramSearch();
+						} else return pr.getTelegramSearchQuery();
 
-							@Override
-							public void setObject(ODocument value) {
-
-							}
-						};
-					}
-
-					@Override
-					protected IModel<String> newLabelModel() {
-						return new SimpleNamingModel<String>("class.bot."+ getPropertyObject());
 					}
 
 					@Override
 					protected Component resolveComponent(String id, DisplayMode mode, String critery) {
-						if(DisplayMode.EDIT.equals(mode) && critery.equals("isEnableToSearch")) {
-							return new CheckBox(id);
-						} else if (DisplayMode.EDIT.equals(mode) && critery.equals("searchQuery")) {
-							return new TextArea<ODocument>(id);
-						} else{
-							return new ODocumentPageLink(id, getValueModel()).setDocumentNameAsBody(true);
+						if (DisplayMode.EDIT.equals(mode) && !OSecurityHelper.isAllowed(ORule.ResourceGeneric.SCHEMA, null, OrientPermission.UPDATE)) {
+							mode = DisplayMode.VIEW;
+						}
+						if (DisplayMode.VIEW.equals(mode)) {
+							if (critery.equals(propertiesList.get(0))) {
+								return new BooleanViewPanel(id, Model.<Boolean>of(getModel())).setHideIfFalse(true);
+							} else return new Label(id, getModel());
+						} else if (DisplayMode.EDIT.equals(mode)){
+							if (critery.equals(propertiesList.get(0))) {
+								return new BooleanEditPanel(id, Model.<Boolean>of(getModel()));
+							} else return new TextField(id, getModel());
+						}
+						return null;
+					}
+
+					@Override
+					protected void setValue(OClass entity, String critery, Object value) {
+						if (critery.equals(propertiesList.get(0))) {
+							pr.setTelegramSearch((Boolean) value);
+						} else if (critery.equals(propertiesList.get(1))) {
+							pr.setTelegramSearchQuery((String) value);
 						}
 					}
+
+
 				};
 			}
 		};
+
 		structureTable.addCommand(new EditSchemaCommand<OClass>(structureTable, getModeModel()));
 		structureTable.addCommand(new SaveSchemaCommand<OClass>(structureTable, getModeModel()));
 		form.add(structureTable);
 		add(form);
+	}
+
+	private class OBotWidgetPopertiesClass implements Serializable {
+		private boolean telegramSearch;
+		private String telegramSearchQuery;
+		private final String className;
+		private final String docName;
+		private static final long serialVersionUID = 2L;
+
+		public OBotWidgetPopertiesClass(final String className, final String docName) {
+			ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+			ORecordIteratorClass<ODocument> oDocuments = db.browseClass(className);
+			this.className = className;
+			this.docName = docName;
+			for (ODocument doc : oDocuments) {
+				if (doc.field("name").equals(docName)) {
+					telegramSearch = doc.field(propertiesList.get(0), OType.BOOLEAN);
+					telegramSearchQuery = doc.field(propertiesList.get(1), OType.STRING);
+				}
+			}
+		}
+
+		public boolean isTelegramSearch() {
+			return telegramSearch;
+		}
+
+		public String getTelegramSearchQuery() {
+			return telegramSearchQuery;
+		}
+
+		public void setTelegramSearch(boolean isTelegramSearch) {
+			telegramSearch = isTelegramSearch;
+			ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+			db.commit();
+			for (ODocument doc : db.browseClass(className)) {
+				if (doc.field("name").equals(docName)) {
+					doc.field(propertiesList.get(0), telegramSearch);
+					doc.save();
+					break;
+				}
+			}
+			db.commit();
+		}
+
+		public void setTelegramSearchQuery(String telegramSearchQuery) {
+			this.telegramSearchQuery = telegramSearchQuery;
+			ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+			db.commit();
+			for (ODocument doc : db.browseClass(className)) {
+				if (doc.field("name").equals(docName)) {
+					doc.field(propertiesList.get(1), telegramSearchQuery);
+					doc.save();
+					break;
+				}
+			}
+			db.commit();
+		}
+
+	}
+
+	void modifySchema() {
+		final String classNameParent = "OClassTelegramState";
+		ODatabaseDocument db = OrientDbWebSession.get().getDatabase();
+		OSchema schema = db.getMetadata().getSchema();
+		db.commit();
+		if (schema.existsClass(classNameParent)) schema.dropClass(classNameParent);
+		if (!schema.existsClass(classNameParent)) {
+			OClass oClass = schema.createClass(classNameParent);
+			oClass.createProperty("name", OType.STRING);
+			oClass.createProperty("telegramSearch", OType.BOOLEAN);
+			oClass.createProperty("telegramSearchQuery", OType.STRING);
+			Collection<OClass> classes = schema.getClasses();
+			List<String> classNames = new ArrayList<>();
+			for (OClass oClass1 : classes) {
+				classNames.add(oClass1.getName());
+			}
+			Collections.sort(classNames);
+			for (String className : classNames) {
+				ODocument oDocument = new ODocument();
+				oDocument.field("name", className);
+				oDocument.field("telegramSearch", true);
+				oDocument.field("telegramSearchQuery", "query123");
+				oDocument.setClassName(classNameParent);
+				oDocument.save();
+				db.commit();
+			}
+		}
+		db.commit();
 	}
 
 	private void test() {
