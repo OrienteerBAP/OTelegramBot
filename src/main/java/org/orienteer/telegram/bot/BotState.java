2435af3b0e7279bccd8c6cc5be6fbd88b87957aa
Vitaliy Gonchar <weaxme@gmail.com>
2016-11-12 03:08:36 +0200

    Delete redundant state

--- src/main/java/org/orienteer/telegram/bot/BotState.java
+++ src/main/java/org/orienteer/telegram/bot/BotState.java
@@ -4,45 +4,22 @@ package org.orienteer.telegram.bot;
  * @author Vitaliy Gonchar
  */
 enum BotState {
-    START("/start"),
     NEW_SEARCH(""),
     NEW_GLOBAL_SEARCH(BotMessage.NEW_GLOBAL_SEARCH_BUT),
     NEW_CLASS_SEARCH(BotMessage.NEW_CLASS_SEARCH_BUT),
     CLASS_SEARCH("/classMenuOptions"),
     BACK(BotMessage.BACK),
-
-    GLOBAL_FIELD_NAMES_SEARCH_BUT(BotMessage.FIELD_NAMES_BUT),
-    GLOBAL_FIELD_VALUES_SEARCH_BUT(BotMessage.FIELD_VALUES_BUT),
-    GLOBAL_DOC_NAMES_SEARCH_BUT(BotMessage.DOC_NAMES_BUT),
-
-    CLASS_MENU_SEARCH_BUT(BotMessage.CLASS_SEARCH_BUT),
-    CLASS_MENU_CHANGE_CLASS_BUT("/changeClass"),
-
-    CLASS_FIELD_NAMES_SEARCH_BUT(BotMessage.CLASS_FIELD_NAMES_BUT),
-    CLASS_FIELD_VALUES_SEARCH_BUT(BotMessage.CLASS_FIELD_VALUES_BUT),
-    CLASS_DOC_NAMES_SEARCH_BUT(BotMessage.CLASS_DOC_NAMES_BUT),
-
-
-    BACK_TO_MAIN_MENU(BotMessage.BACK_TO_MAIN_MENU_BUT),
-    BACK_TO_CLASS_SEARCH(BotMessage.BACK_TO_CLASS_SEARCH_BUT),
     GO_TO_DOCUMENT("/_"),
     GO_TO_CLASS("/"),
-
     NEXT_RESULT(BotMessage.NEXT_RESULT_BUT),
     PREVIOUS_RESULT(BotMessage.PREVIOUS_RESULT_BUT),
-
     SEARCH_GLOBAL("/globalSearch"),
-
-    SEARCH_GLOBAL_FIELD_NAMES("/searchGlobalFieldNames"),
-    SEARCH_GLOBAL_FIELD_VALUES("/searchGlobalFieldValues"),
-    SEARCH_GLOBAL_DOC_NAMES("/searchGlobalDocNames"),
-
     SEARCH_IN_CLASS_GLOBAL("/searchInClassAll"),
+    ERROR("/error"),
 
-    SEARCH_CLASS_FIELD_NAMES("/searchClassFieldNames"),
-    SEARCH_CLASS_FIELD_VALUES("/searchClassFieldValues"),
-    SEARCH_CLASS_DOC_NAMES("/searchClassDocNames"),
-    ERROR("/error");
+    START("/start"),
+    LANGUAGE("/language"),
+    ABOUT("/about");
 
     String command;
 
