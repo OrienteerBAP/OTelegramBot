d9937996de9ed08acaa3b11610c462144322bace
Vitaliy Gonchar <weaxme@gmail.com>
2016-11-25 21:33:47 +0200

    Optimize imports

--- src/main/java/org/orienteer/telegram/OTelegramBotApplication.java
+++ src/main/java/org/orienteer/telegram/OTelegramBotApplication.java
@@ -1,13 +1,9 @@
 package org.orienteer.telegram;
 
 import org.orienteer.core.OrienteerWebApplication;
-import org.orienteer.core.OrienteerWebSession;
-import org.orienteer.telegram.bot.BotMessage;
 import org.orienteer.telegram.module.OTelegramModule;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.Locale;
 
 public class OTelegramBotApplication extends OrienteerWebApplication
 {
